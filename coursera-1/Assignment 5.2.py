c6de950466b35ddda8e69f396adb64682cd5c8f8
Joan A <joan_ake@yahoo.com>
2016-03-31 23:33:53 +0800

    Update Assignment 5.2.py

--- coursera-1/Assignment 5.2.py
+++ coursera-1/Assignment 5.2.py
@@ -5,26 +5,25 @@
 ## If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
 ## Enter the numbers from the book for problem 5.1 and Match the desired output as shown.
 
+num_list = []
 largest = None
 smallest = None
-    
+
 while True:
-    num = raw_input("Enter a number: ")
-    if num == "done" : break
-    
-    try: 
-        num = float(num)
-    
+    inp = raw_input("Enter a number: ")
+    if inp == "done" : break
+    try:
+        num = int(inp)
+	num_list.append(num)
     except:
-        print 'Invalid input'
-        continue 
-    
-    if smallest is None:
-    	smallest = num
+        print "Invalid input"
+    continue
+        
+for num in num_list:
+	if largest<num or largest == None:
+		largest = num 
+	if smallest>num or smallest == None:
+		smallest = num 
 
-    if num > largest:
-    	largest = num
-
-
-print "Maximum is", int(largest)
-print "Minimum is", int(smallest)
+print 'Maximum is', largest
+print 'Minimum is', smallest
