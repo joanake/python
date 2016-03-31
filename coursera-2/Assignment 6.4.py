a5870df451daf3618a2bf54f566548d8e7ca8bd6
Joan A <joan_ake@yahoo.com>
2016-03-31 23:35:23 +0800

    Update Assignment 6.4.py

--- coursera-2/Assignment 6.4.py
+++ coursera-2/Assignment 6.4.py
@@ -1,10 +1,8 @@
-# It is currently 16.86 days past the due date (2015-07-14) so your late penalty is 100 percent.
-
-## 6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. 
-## Convert the extracted value to a floating point number and print it out.
+'''6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. 
+Convert the extracted value to a floating point number and print it out.'''
 
 text = "X-DSPAM-Confidence:    0.8475";
-found = text.find('0')
-found_2 = text.find('5')+1
-x = text[found:found_2]
-print float(x)
+num = text.find('0') 	#Find starting position of 0.8475
+num = text[num:]		#String slicing of 0.8475
+num = float(num)		#Convert to floating point number
+print num				
